34545096c9b9ca6e22dd69ece407c7fb8b115c92
brandeeogunjobi <ketaloginc@brandees-MacBook-Pro.local>
2014-10-16 20:27:31 -0700

    I made changes to the repo js and html.

---
+++ json.js
@@ -0,0 +1,48 @@
+//JSON
+
+var json = {
+	"projects": {
+		"000011": {
+			"name": "Week 1",
+			"date": 4
+		},
+		"000012": {
+			"name": "Week 2",
+			"date": 10
+		},
+		"000013": {
+			"name": "Week 3",
+			"date": 16
+		}
+	}
+};
+
+/*for (var key in json.projects) {
+	var projects = json.projects[key];
+	//projects.name
+};*/
+
+var json2 = {
+	"projects" : [
+	{
+		"vvid": "000011",
+		"name": "Week 1",
+		"date": 4
+	},
+	{
+		"vvid": "000012",
+		"name": "Week 2",
+		"date": 10
+	},
+	{
+		"vvid": "000013",
+		"name": "Week 3",
+		"date": 16
+	}
+	
+	]
+};
+
+
+
+
